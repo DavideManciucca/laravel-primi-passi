92f963499d8d6bb65347e7d87b21ac6c2eda9f6e
DavideManciucca <davidemanciucca@gmail.com>
2022-06-23 09:23:56 +0200

    esercizio terminato

---
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat nisi quos mollitia neque error optio hic deserunt. Sapiente explicabo, dignissimos ea optio eligendi doloribus architecto ipsum, recusandae ducimus labore non!
+
+    <h1>Buongiorno {{$utente}}</h1>
+</body>
+</html>
--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $utente = 'Davide';
+
+    return view('home', ['utente'=>$utente]);
 });
